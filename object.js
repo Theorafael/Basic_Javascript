457c5906051cb3a342310d946982c37fb7bf4cd1
Theorafael <Theorafaeltitahena@gmail.com>
2022-11-01 16:44:16 +0700

    1-11-2922

---
+++ object.js
@@ -0,0 +1,46 @@
+/*var person ={
+    nama : "Anton",
+    age : 32,
+    favcolor : "Hitam",
+    height : 170,
+    weight : 69
+}
+if(person.weight > 64){var msg = 'obesitas'}else{var msg = 'ideal'}
+alert(person.nama + 'anton tambah' +person ['weight'] + 'beratnya' +msg)
+
+console.log(person.nama;)
+*/
+
+
+//  Lesson 25.1
+/*var course = {nama: "push rank", chapter:1}
+console.log(course.nama.length)*/
+
+
+// Lesson 26
+// instance of Object (new)
+/*
+function person(name, age, color){
+    this.name = name
+    this.age = age
+    this.favColor = color
+}
+var p1 = new personalbar("Ncunk", 27, "Cream");
+var p2 = new personalbar("Ncang", 17,"brown");
+console.log(p1.name + 'dan' + p2.name + 'menyukai warna' + p2.favColor)
+*/
+
+// Lesson 34.1
+/*
+function makan (status, food{
+    this.status = status
+    this.food = food
+    this.changeName = function(status){
+        this.status = status
+    }
+}
+
+var p1 = new makan('ahtong', 'opor');
+p1.changestatus('selesai makan')
+console.log(p1.status + "madang" + p1.)
+*/
---
+++ object_2.js
@@ -0,0 +1,63 @@
+// 31.2
+function person(length, width, height) {
+    return console.log(length * width * height)
+}
+
+person(15, 100, 200)
+
+// 32.2
+function main() {
+    
+    var flightNumber = "NGT 929";
+    var flightStatus = "Landed";
+    var flight1= new Flight(flightNumber,
+    flightStatus);
+    console.log('The flight ' + flight1.number
+    + ' is ' + flight1.status);
+}
+function Flight(flightNumber, status) {
+    this.number = flightNumber;
+    this.status = status;
+};
+main()
+
+
+// 34.2
+function main() {
+    var prodID = "LD1493";
+    var price = 1700;
+    var discount = 15;
+
+    var prod1= new Product(prodID, price);
+    console.log(prod1.prodID + " price: " + prod1.price);
+
+    prod1.changePrice(discount);
+    console.log(prod1.prodID + " new price: " + prod1.price);
+}
+
+function Product(prodID, price) {
+    this.prodID = prodID;
+    this.price = price;
+
+    this.changePrice = function(discount) {
+
+        return this.price = this.price - ((discount/100) * this.price );   
+    }
+}
+main()
+
+
+
+// 36
+function contact(name, number) {
+    this.name = name;
+    this.number = number;
+    this.print = print; 
+    } 
+    function print() { 
+console.log(this.name + ": " + this.number);
+}
+var a = new contact("David", 12345);
+var b = new contact("Amy", 987654321);
+a.print();
+b.print();
